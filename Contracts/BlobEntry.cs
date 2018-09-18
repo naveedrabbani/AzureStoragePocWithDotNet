bb5e313fed253015cad2d17c0b0a76305de00f1c
naveedrabbani <43229052+naveedrabbani@users.noreply.github.com>
2018-09-18 11:30:19 -0500

    Add files via upload

---
+++ Contracts/BlobEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+[DataContract]
+public class BlobEntry
+{
+    [JsonProperty(PropertyName = "blobContainer")]
+    public string BlobContainer { get; set; }
+
+    /// <summary>
+    /// Description
+    /// </summary>
+    [JsonProperty(PropertyName = "fileName")]
+    public string FileName { get; set; }
+
+    /// <summary>
+    /// Description
+    /// </summary>
+    [JsonProperty(PropertyName = "action")]
+    public string Action { get; set; }
+}
