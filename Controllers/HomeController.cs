696fa886dc16ea96dde07154f05d929e3b588d60
naveedrabbani <43229052+naveedrabbani@users.noreply.github.com>
2018-09-18 00:47:26 -0500

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -129,7 +129,7 @@ namespace AzureQueuePocWithDotNet.Controllers
             CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(localFileName);
             cloudBlockBlob.DownloadToFileAsync(destinationFile, FileMode.Create);
 
-            return Ok("Downloaded blob: " + localFileName);
+            return Ok("Downloaded blob to " + sourceFile);
         }
 
         public IActionResult Index()
