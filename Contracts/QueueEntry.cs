e1b5df1d6cbdddfa496655a8e4db77ddf14b369f
naveedrabbani <43229052+naveedrabbani@users.noreply.github.com>
2018-09-18 11:12:46 -0500

    Add files via upload

---
+++ Contracts/QueueEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+[DataContract]
+public class QueueEntry
+{
+    [JsonProperty(PropertyName = "queueName")]
+    public string QueueName { get; set; }
+
+    /// <summary>
+    /// Description
+    /// </summary>
+    [JsonProperty(PropertyName = "queueMessage")]
+    public string QueueMessage { get; set; }
+
+    /// <summary>
+    /// Description
+    /// </summary>
+    [JsonProperty(PropertyName = "action")]
+    public string Action { get; set; }
+}
